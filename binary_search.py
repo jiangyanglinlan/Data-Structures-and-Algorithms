bb55cc25aa9f4127ec40fd3a79c10e3149623e9e
jiangyanglinlan <1815212328@qq.com>
2018-04-26 22:12:59 +0800

    二分查找

---
+++ binary_search.py
@@ -0,0 +1,23 @@
+def binary_search(list, item):
+	low = 0
+	high = len(list) - 1
+	
+	while low <= high:
+		mid = (int)((low + high) / 2)
+		guess = list[mid]
+		if guess == item:
+			return mid
+		if guess > item:
+			high = mid - 1
+		else:
+			low = mid + 1
+	return None
+	
+
+my_list = [1, 3, 5, 7, 9, 11, 13, 15]
+
+print(binary_search(my_list, 11))
+
+print(binary_search(my_list, 7))
+
+print(binary_search(my_list, 6))
