2c36d24b6d5dc4a59d8a57ce7de70188fa4ddf3b
jiangyanglinlan <1815212328@qq.com>
2018-05-13 22:31:04 +0800

    add leetcode notes

---
+++ leetcode/1_Two_Sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        dict = {}
+        for i in range(len(nums)):
+            x = nums[i]
+            #print('{}'.format(dict))
+            if target-x in dict:
+                return [dict[target-x], i]
+            dict[x] = i
