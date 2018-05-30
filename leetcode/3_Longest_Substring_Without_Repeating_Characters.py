cd59295b89f1de7ea8e47950e7012c5805090193
jiangyanglinlan <1815212328@qq.com>
2018-05-30 23:10:45 +0800

    leetcode 第 3 题

---
+++ leetcode/3_Longest_Substring_Without_Repeating_Characters.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def lengthOfLongestSubstring(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        start = 0
+        max_len = 0
+        dict = {}
+        for i in range(len(s)):
+            dict[s[i]] = -1
+        for i in range(len(s)):
+            if dict[s[i]] != -1:
+                while start <= dict[s[i]]:
+                    start += 1
+
+            if i - start + 1 > max_len:
+                max_len = i - start + 1
+
+            dict[s[i]] = i
+        return max_len
