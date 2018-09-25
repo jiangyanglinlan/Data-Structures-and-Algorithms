aa5fff344d3990e9561d24baf491425eac5b4f64
jiangyanglinlan <1815212328@qq.com>
2018-09-25 19:25:19 +0800

    leetcode 第 46 题

---
+++ leetcode/46_Permutations.py
@@ -0,0 +1,25 @@
+class Solution:
+    def permute(self, nums):
+        result = []
+        if nums is None or len(nums) == 0:
+            return []
+        l = []
+        self.permute_helper(result, l, nums)
+        return result
+
+
+    def permute_helper(self, result, l, nums):
+        print(l)
+        if len(l) == len(nums):
+            new_list = l.copy()
+            result.append(new_list)
+        for i in range(0, len(nums)):
+            if nums[i] in l:
+                continue
+            l.append(nums[i])
+            self.permute_helper(result, l, nums)
+            l.pop(len(l) - 1)
+
+
+s = Solution()
+print(s.permute([1, 2, 3]))
