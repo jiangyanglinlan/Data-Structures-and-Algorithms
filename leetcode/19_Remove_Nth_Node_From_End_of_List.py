118c69b358da3574b7691a175005f7714d6df78f
jiangyanglinlan <1815212328@qq.com>
2018-11-25 16:36:05 +0800

    leetcode 第 19 题, 删除链表倒数第 N 个节点

--- leetcode/19_Remove_Nth_Node_From_End_of_List.py
+++ leetcode/19_Remove_Nth_Node_From_End_of_List.py
@@ -11,24 +11,19 @@ class Solution:
         :type n: int
         :rtype: ListNode
         """
-        pre_point = head  # 记录要删除的节点的前一个节点
         slow_point = head
         fast_point = head
         while n > 0:
             fast_point = fast_point.next
             n -= 1
 
-        flag = False
-        while fast_point is not None:
-            slow_point = slow_point.next
+        if fast_point is None:
+            return head.next
+
+        while fast_point.next is not None:
             fast_point = fast_point.next
-            if flag is True:
-                pre_point = pre_point.next
-            flag = True
+            slow_point = slow_point.next
 
-        if pre_point == slow_point: # 出现此情况的输入举例: ([1], 1)、([1, 2], 2)
-            return pre_point.next
+        slow_point.next = slow_point.next.next
 
-        pre_point.next = slow_point.next
-        slow_point.next = None
         return head
