6da86ed7c81d9080403d0939c3c2201e24101cbe
jiangyanglinlan <1815212328@qq.com>
2018-12-05 22:46:34 +0800

    快速排序

--- algorithms/quick_sort.py
+++ algorithms/quick_sort.py
@@ -11,6 +11,27 @@ def quick_sort(array):
         return quick_sort(less) + [pivot] + quick_sort(greater)
 
 
+def quick_sort2(array, begin, end):
+    if begin >= end:
+        return
+    pivot_postion = partition(array, begin, end)
+    quick_sort2(array, begin, pivot_postion - 1)
+    quick_sort2(array, pivot_postion + 1, end)
+
+
+def partition(nums, begin, end):
+    pivot = nums[begin]
+    while begin < end:
+        while begin < end and nums[end] >= pivot:
+            end -= 1
+        nums[begin] = nums[end]
+        while begin < end and nums[begin] <= pivot:
+            begin += 1
+        nums[end] = nums[begin]
+    nums[begin] = pivot
+    return begin
+
+
 def random_int_list(start, stop, length):
     start, stop = (int(start), int(stop)) if start <= stop else (int(stop), int(start))
     length = int(abs(length)) if length else 0
@@ -23,3 +44,5 @@ def random_int_list(start, stop, length):
 if __name__ == '__main__':
     l = random_int_list(1,10000, 20)
     print(quick_sort(l))
+    quick_sort2(l, 0, len(l) - 1)
+    print(l)
